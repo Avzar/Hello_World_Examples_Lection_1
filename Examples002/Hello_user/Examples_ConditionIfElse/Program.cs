e38a0d45bd95ae5e1309f14cd943e9e82753bc69
Avzar <rassstinson@gmail.com>
2023-02-22 12:40:29 +0600

    .\Examples_ConditionIfElse/Program.cs added

---
+++ Examples002/Hello_user/Examples_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "avzar")
+{
+    Console.WriteLine("!!! Hello Avzar!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
